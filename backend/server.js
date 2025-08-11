09a3dbdcf221d092c4b639163d0b4a3e4a8e740f
manis-h <85171340+manis-h@users.noreply.github.com>
2025-08-12 00:26:02 +0530

    ss

--- backend/server.js
+++ backend/server.js
@@ -49,7 +49,7 @@ app.use((err, req, res, next) => {
     message: 'Something broke!' 
   });
 });
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
